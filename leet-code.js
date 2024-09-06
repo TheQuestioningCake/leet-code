e584aec8ae3a552a5817f3f0e7b31f54390f9f3a
TheQuestioningCake <perrywayne39@gmail.com>
2024-09-06 13:39:23 -0400

    added roman to integer algorithm

--- leet-code.js
+++ leet-code.js
@@ -1,3 +1,6 @@
+// Easy leet-code problems
+
+
 // @param {number []} nums
 // @param {number} target
 // @return {number[]}
@@ -34,3 +37,31 @@ function isNumPalindrome (num) {
         return false
     }
 };
+
+
+// function to convert roman to integer
+
+function romanToInteger(s) {
+    romanSymbols = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    value = 0
+
+    for( let i = 0; i < s.length;  i += 1) {
+        if (i < s.length - 1 && romanSymbols[s[i]] < romanSymbols[s[i + 1]]) {
+            value -= romanSymbols[s[i]];
+        } else {
+            value += romanSymbols[s[i]];
+        }
+    }
+
+    return value
+    
+}
