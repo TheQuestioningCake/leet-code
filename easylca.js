5224b49181361a17f42ee6919aeabe57ad1bafdd
TheQuestioningCake <perrywayne39@gmail.com>
2024-09-09 12:50:55 -0400

    added algorithm for longest common prefix answer

--- easylca.js
+++ easylca.js
@@ -65,3 +65,28 @@ function romanToInteger(s) {
     return value
     
 }
+
+
+// function for longest common prefix
+
+function longestCommonPrefix(strs) {
+
+    if (strs.length === 0) {
+        return ''
+    }
+
+let prefix = strs[0]
+
+ for (let i = 0; i < strs.length; i++) {
+   const currentString = strs[i]
+   while (currentString.indexOf(prefix) !== 0) {
+    prefix = prefix.substring(0, prefix.length -1);
+    if  (prefix === '') {
+        return ''
+    }
+   }
+ }
+
+ return prefix;
+
+}
