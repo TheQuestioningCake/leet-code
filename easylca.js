2c48124a1b5edd6737a77a906505b5d5788f833f
TheQuestioningCake <perrywayne39@gmail.com>
2024-09-11 14:21:59 -0400

    added mergeTwoList algorithm

--- easylca.js
+++ easylca.js
@@ -116,3 +116,27 @@ function validParentheses (s) {
 
     return parenthesesArray.length === 0;
 }
+
+
+// function to merge two sorted list
+function mergeTwoList(list1, list2) {
+    let tempNode = new ListNode(0, null)
+    let currentNode = tempNode
+
+    while (list1 && list2) {
+        if(list1.val < list2.val) {
+            currentNode.next = list1;
+            list1 = list1.next
+        } else {
+            currentNode.next = list2;
+            list2 = list2.next
+        }
+
+        currentNode = currentNode.next
+    }
+
+    currentNode.next = list1 || list2
+
+    return tempNode.next;
+    
+};
